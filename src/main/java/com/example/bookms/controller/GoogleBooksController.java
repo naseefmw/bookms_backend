31148eb3aa15c26deb7876caf93c8325d6cdfe67
naseefmw <60293453+naseefmw@users.noreply.github.com>
2025-01-10 10:30:00 +0530

    using gbooks api key

--- src/main/java/com/example/bookms/controller/GoogleBooksController.java
+++ src/main/java/com/example/bookms/controller/GoogleBooksController.java
@@ -1,5 +1,6 @@
 package com.example.bookms.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,11 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class GoogleBooksController {
+    @Value("${gbooksapi.key}")
+    String apikey;
     @GetMapping(value = "/gbooks/{isbn}")
     public Object getBookDetails(@PathVariable("isbn") String isbn) {
-        String uri = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn;
+        String uri = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn + "&key=" + apikey;
         RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject(uri, Object.class);
     }
