8e6afa36ba5d123a836958d4d9c467cac5a89b85
naseefmw <60293453+naseefmw@users.noreply.github.com>
2025-01-17 09:05:07 +0530

    validation message edit

--- src/main/java/com/example/bookms/model/BookInput.java
+++ src/main/java/com/example/bookms/model/BookInput.java
@@ -10,18 +10,20 @@ import java.time.LocalDate;
 public class BookInput {
 
     @NotNull
-    @Size(min = 1,max=100, message = "Title length must be less than 100")
+    @Size(max=100, message = "Title length must be less than or equal to 100")
+    @Size(min = 1, message = "Title is required")
     private String title;
 
     @NotNull
-    @Size(min = 1, max = 50, message = "Author length must be less than 50")
+    @Size(max = 50, message = "Author length must be less than or equal to 50")
+    @Size(min = 1, message = "Author is required")
     private String author;
 
     @NotNull
     private LocalDate pub_date;
 
     @NotNull
-    @Pattern(regexp = "^\\d{13}$", message = "Must be 13 digit number")
+    @Pattern(regexp = "^\\d{13}$", message = "Must be a 13 digit number")
     private String isbn;
 
     private String genre;
